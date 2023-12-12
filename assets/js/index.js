6797521e8aaa0d198a7b0679b8e6606bf03d8166
Antonio Marucci <antoniomarucci13@gmail.com>
2023-12-12 17:01:42 +0100

    ancore colori

--- assets/js/index.js
+++ assets/js/index.js
@@ -88,8 +88,8 @@ const createCard = (obj, cardType) => {
     <i class="bi bi-play-circle-fill position-absolute positionCustom fs-1"></i>
     </div>
     <div class="card-body fix-h-100 ">
-      <h6 class="card-title overflowCustom max-h-50 fs-7"><a href="./album.html?idAlbum=${obj.album.id}">${obj.album.title}</a></h6>
-      <p class="card-text fs-8"><a href="./artist.html?idAlbum=${obj.album.id}">${obj.artist.name}</a></p>
+      <h6 class="card-title overflowCustom max-h-50 fs-6"><a class="customColorA" href="./album.html?idAlbum=${obj.album.id}">${obj.album.title}</a></h6>
+      <p class="card-text fs-8"><a class="customColorA" href="./artist.html?idArtist=${obj.artist.id}&idAlbum=${obj.album.id}">${obj.artist.name}</a></p>
     </div>
   </div>`;
     return card;
