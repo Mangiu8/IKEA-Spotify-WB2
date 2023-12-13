eb088380d24c182208e974f1107017571356e638
Antonio Marucci <antoniomarucci13@gmail.com>
2023-12-13 14:21:34 +0100

    pull

---
+++ assets/js/artist.js
@@ -0,0 +1,116 @@
+import { apiKey } from "./apiKey.js";
+
+const parameters = new URLSearchParams(window.location.search);
+const nomeArtist = parameters.get("idArtist");
+const apiUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + nomeArtist;
+const idAlbum = parameters.get("idAlbum");
+const apiUrlAlbum = "https://deezerdevs-deezer.p.rapidapi.com/album/" + idAlbum;
+
+// Funzione per ottenere la lista di playlist
+async function getArtist() {
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+        "X-RapidAPI-Key": apiKey,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Errore nella richiesta: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log("Lista di playlist:", data);
+    generateCardList(data);
+  } catch (error) {
+    console.error("Si è verificato un errore:", error.message);
+  }
+}
+// Funzione per ottenere la lista di playlist
+async function getAlbum() {
+  try {
+    const response = await fetch(apiUrlAlbum, {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+        "X-RapidAPI-Key": apiKey,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Errore nella richiesta: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log("Lista di playlist:", data);
+    showArtist(data);
+  } catch (error) {
+    console.error("Si è verificato un errore:", error.message);
+  }
+}
+
+const showArtist = (data) => {
+  const titolo = document.getElementById("nomeArtista");
+  const banner = document.getElementById("sfondoArtista");
+  const fans = document.getElementById("fans");
+  console.log("cose");
+  console.log(data);
+  fans.innerHTML = `Ascoltatori mensili ${data.fans}`;
+  banner.style.backgroundImage = "url(" + data.artist.picture_xl + ")";
+  titolo.innerText = data.artist.name;
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  getAlbum();
+  getArtist();
+});
+
+const generateCardList = (obj) => {
+  const cardContainer = document.getElementById("Salvatore");
+  const tracks = obj.data;
+  cardContainer.innerHTML = "";
+  console.log(tracks);
+  for (let i = 0; i < 5; i++) {
+    let card = createCard(tracks[i], i + 1);
+    cardContainer.appendChild(card);
+    console.log(tracks[i]);
+  }
+};
+
+const createCard = (obj, index) => {
+  console.log(obj);
+  const card = document.createElement("div");
+  card.className = "row";
+  card.innerHTML = ` 
+  <div class="col-8 text-light my-2">
+  <div class="d-flex gap-2 align-items-center">
+    <p class="mx-1">${index}</p>
+    <img
+      aria-hidden="false"
+      draggable="false"
+      loading="eager"
+      src="${obj.album.cover_small}"
+      alt=""
+      class="mMx2LUixlnN_Fu45JpFB rkw8BWQi3miXqtlJhKg0 Yn2Ei5QZn19gria6LjZj"
+      width="40"
+      height="40"
+      style="border-radius: 4px"
+    />
+    <p>${obj.title}</p>
+  </div>
+</div>
+<div class="col-2 text-light">
+  <div>
+    <p>${obj.rank}</p>
+  </div>
+</div>
+<div class="col-2 text-light">
+  <div>
+    <p>${obj.duration}</p>
+  </div>
+</div>
+`;
+  return card;
+};
